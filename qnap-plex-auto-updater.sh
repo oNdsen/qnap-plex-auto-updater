b4111dd849150074a7cfdf5b7cf950cd79621751
oNdsen <a.reinhard@me.com>
2022-02-23 16:56:59 +0100

    Bash Changes for QTS 5.0.0
    Also changed the "latest" Content from URL to "current" with Version Number and did some change with the jq filter and added some Debug Outputs

--- qnap-plex-auto-updater.sh
+++ qnap-plex-auto-updater.sh
@@ -1,25 +1,59 @@
 #!/bin/bash
 
-current_dir=$(dirname "$0")
-last=$( cat ${current_dir}/last)
-echo "installed: ${last}"
+# enable or disable debug output
+debug=$false
 
-plex_token=$(cat /share/PlexData/Plex\ Media\ Server/Preferences.xml | grep -oP 'PlexOnlineToken="\K[^"]+')
-plex_token=$(cat /share/PlexData/Plex\ Media\ Server/Preferences.xml | grep -oE 'PlexOnlineToken="[^"]+"' | sed -E 's/PlexOnlineToken="([^"]+)"/\1/')
-plex_url="https://plex.tv/api/downloads/5.json?channel=plexpass&X-Plex-Token=${plex_token}"
-echo "Plex URL: ${plex_url}"
-jq_filter=".nas.QNAP.releases[] | select(.build==\"linux-x86_64\") | .url"
-latest_json=$(curl -sS "${plex_url}")
-latest=$(echo "${latest_json}" | jq -r "${jq_filter}")
-if [ "$last" != "$latest" ]; then
-    echo "new version $latest available (prev: $last). Downloading it."
-    curl "${latest}" -o ${current_dir}/plex.qpkg
-    sh ${current_dir}/plex.qpkg
-    rm ${current_dir}/plex.qpkg
-    echo ${latest} > ${current_dir}/last
-    echo "done"
+# get current directory
+scriptDir=$(dirname "$0")
+
+# get current downloaded plex server version
+currentVersion=$(cat "$scriptDir/current")
+if [[ $debug ]]
+then
+	echo "Installed: $currentVersion"
+fi
+
+# get plex token from preferences and build url
+plex_token=$(cat "/share/PlexData/Plex Media Server/Preferences.xml" | grep -oE 'PlexOnlineToken="[^"]+"' | sed -E 's/PlexOnlineToken="([^"]+)"/\1/')
+plex_url="https://plex.tv/api/downloads/5.json?channel=plexpass&X-Plex-Token=$plex_token"
+if [[ $debug ]]
+then
+	echo "Plex URL: $plex_url"
+fi
+
+# get latest data from plex
+latestVersion=$(echo $(curl -s $plex_url) | jq ".nas.QNAP | .version" | tr -d '"')
+latestQPKG=$(echo $(curl -s $plex_url) | jq ".nas.QNAP.releases[] | select(.build==\"linux-x86_64\") | .url" | tr -d '"')
+if [[ $debug ]]
+then
+	echo "Latest Version: $latestVersion"
+	echo "QPKG:           $latestQPKG"
+fi
+
+# compare currentVersion and plex version
+if [[ $currentVersion != $latestVersion ]]
+then
+    echo "New Plex Media Server Version detected"
+	echo "  Latest:  $latestVersion"
+	echo "  Current: $currentVersion"
+	echo ""
+    
+	echo "Downloading from: $latestQPKG"
+    curl "$latestQPKG" -s -o "$scriptDir/plex.qpkg"
+	
+	echo "Installing Version $latestVersion"
+    sh "$scriptDir/plex.qpkg"
+    
+	echo "Install done. Removing downloaded File"
+    rm "$scriptDir/plex.qpkg"
+    
+	echo "Updating Version DB to $latestVersion"
+    echo $latestVersion > $scriptDir/current
+	
+	echo ""
+    echo "Plex Media Server Update done."
 else
-    echo "same versions"
-    echo $latest
-    echo $last
+    echo "No new Plex Media Server Version detected"
+    echo "  Latest:  $latestVersion"
+	echo "  Current: $currentVersion"
 fi
